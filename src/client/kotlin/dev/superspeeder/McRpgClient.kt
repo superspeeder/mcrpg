512f1da9e93fd451b7c8f72d2fd6b328a6618300
superspeeder <andy@delusion.org>
2025-02-22 18:30:44 -0800

    Create project from template

---
+++ src/client/kotlin/dev/superspeeder/McRpgClient.kt
@@ -0,0 +1,9 @@
+package dev.superspeeder
+
+import net.fabricmc.api.ClientModInitializer
+
+object McRpgClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/dev/superspeeder/McRpgDataGenerator.kt
@@ -0,0 +1,9 @@
+package dev.superspeeder
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object McRpgDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
